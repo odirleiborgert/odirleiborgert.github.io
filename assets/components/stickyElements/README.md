f9e46f5c321f35e840cefceda2ca21f0f1ae643b
Odirlei Borgert <odirleiborgert@gmail.com>
2016-02-15 21:27:35 -0200

    Add component sticky elements

---
+++ assets/components/stickyElements/README.md
@@ -0,0 +1,30 @@
+# Sticky Elements
+
+Everything can stick. Just because.
+
+[Demo](http://design.iamvdo.me/stickyElements)
+
+## Getting started
+
+`npm install stickyelements` and insert `dist/stickyelements-animate.js` (or build your own bundle using `src` files)
+
+Then, stick elements!
+
+```javascript
+stickyElements('.item', {
+  stickiness: 5,
+  duration: 450
+});
+
+```
+## Arguments
+
+### CSS selector
+
+* Elements that will stick to your mouse
+
+### Options
+
+* `stickiness` [Integer, Object]: How long elements remain stick to your mouse. If integer, apply same `x` and `y` values. If object, can contain `x` and/or `y` key. Integer between 0 and 10. (Default: `3`)
+* `duration` [Integer]: Duration in milliseconds of animation (using [animateplus](https://github.com/bendc/animateplus). (Default: `450`)
+* `pointer` [Boolean]: Enable Pointer Events instead of Mouse Events. Elements will stick to mouse, touch and all input types (Default: `false`). Need [PEP polyfill](https://github.com/jquery/PEP) and `touch-action` attribute on each elements (follow PEP polyfill instructions).
---
+++ assets/components/stickyElements/build.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+var babel  = require('rollup-plugin-babel');
+var rollup = require('rollup');
+var uglify = require('uglify-js');
+
+
+var args = process.argv;
+var packageFile = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+var version = packageFile.version;
+
+var files = {
+  src:         './src/stickyElements.js',
+  srcAnimate:  './node_modules/animateplus/animate.min.js',
+  dist:        './dist/stickyelements.js',
+  distAnimate: './dist/stickyelements-animate.js'
+}
+
+function minify (code) {
+  return uglify.minify(code, {fromString: true}).code;
+}
+
+function runBuild () {
+  fs.mkdir('./dist', (err) => {
+    rollup.rollup({
+      entry: files.src,
+      plugins: [ babel({presets: [ 'es2015-rollup' ]}) ]
+    }).then(function (bundle) {
+      var result = bundle.generate({
+        format: 'iife',
+        moduleName: 'stickyElements'
+      });
+      var code = minify(result.code);
+      fs.writeFile(files.dist, code, 'utf8');
+      fs.readFile(files.srcAnimate, 'utf8', (err, animate) => {
+        fs.writeFile(files.distAnimate, animate + code, 'utf8');
+      });
+    }, function (err) {
+      console.log(err);
+    });
+  });
+}
+
+runBuild();
+
+if (args[2] && args[2] === '-w') {
+  fs.watchFile(files.src, runBuild);
+  fs.watchFile('./src/StickyElement.js', runBuild);
+}
